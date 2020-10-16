0f5c2464e7001d756c480b7db4de3a45ba92c63d
David Bing <davidb.devops@gmail.com>
2020-10-16 12:56:54 -0500

    update main.js: import and invoke getOfficers/useOfficers

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,12 @@
+// import data provider functions
+import { getOfficers, useOfficers } from './officers/OfficerProvider.js'
+
+// debug
 console.log("Welcome to the main module")
+
+// invoke functions
+getOfficers();          // console.table(parsedOfficers)s
+useOfficers();          // array compliments of officers.slice() 
+
+
+
