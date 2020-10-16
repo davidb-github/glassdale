566bb218dc6b4e9da1c7fc8641fba24de58b9a3c
David Bing <davidb.devops@gmail.com>
2020-10-16 13:42:11 -0500

    update CriminalHTML.js: implement CriminalHTML(criminalObj) component

---
+++ scripts/criminals/CriminalHTML.js
@@ -0,0 +1,15 @@
+/**
+ *  CriminalHTML which renders individual criminal objects as HTML
+ */
+export const criminalHTML = (criminalObj) => {
+
+    return `
+        <section class="criminal__card">
+            <div class="criminal__name">${criminalObj.name}</div>
+            <div class="criminal__age">${criminalObj.age}</div>
+            <div class="criminal__conviction">${criminalObj.conviction}</div>
+            <div class="criminal__conviction">${criminalObj.incarceration.start}</div>
+            <div class="criminal__conviction">${criminalObj.incarceration.end}</div>
+        </section>
+    `
+}
