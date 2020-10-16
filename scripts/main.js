656f20fd479c1743504097d692e0db5b7636e66f
David Bing <davidb.devops@gmail.com>
2020-10-16 16:43:19 -0500

    update index.html/main.js: minor formatting change

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,20 @@
 // import data provider functions
+import { CriminalList } from './criminals/CriminalList.js';
+import { getCriminals, useCriminals } from './criminals/CriminalProvider.js';
 import { getOfficers, useOfficers } from './officers/OfficerProvider.js'
 
 // debug
-console.log("Welcome to the main module")
+// console.log("Welcome to the main module")
 
 // invoke functions
-getOfficers();          // console.table(parsedOfficers)s
-useOfficers();          // array compliments of officers.slice() 
+// getOfficers();          // console.table(parsedOfficers)s
+// useOfficers();          // array compliments of officers.slice() 
+
+// getCriminals();
+// criminals = useCriminals();
+// console.log(useCriminals())
+CriminalList();
+
 
 
 
