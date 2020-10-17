d7cfd100717d444e28e0513b6c7eaeed5c6bd394
David Bing <davidb.devops@gmail.com>
2020-10-17 15:16:39 -0500

    update ConfictionProvider.js: implement useConvictions() & getConvictions() components

--- scripts/convictions/ConvictionProvider.js
+++ scripts/convictions/ConvictionProvider.js
@@ -1,11 +1,18 @@
+// init var to hold convictions array built by getConvictions()
 let convictions = []
 
-export const useConvictions = () => convictions.slice()
+// return copy of convictions array
+export const useConvictions = () => {
+    return convictions.slice()
+}
 
+// fetch conviction data and populate convictions array
 export const getConvictions = () => {
-    /*
-        Load database state into application state with a fetch().
-        Make sure the last `then()` sets the local `convictions`
-        variable to what is in the response from the API.
-    */
+    return fetch("https://criminals.glassdale.us/crimes")
+        .then(response => response.json())
+        .then (
+            parsedConvictions => {
+                convictions = parsedConvictions
+            }
+        )
 }
