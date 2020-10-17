a767b663c63c9a14ff8506e283fccf07ea0354e3
David Bing <davidb.devops@gmail.com>
2020-10-17 14:31:59 -0500

    new file/s: ConvictionProvider.js & ConvictionSelect.js - initial commit for these files.

---
+++ scripts/convictions/ConvictionProvider.js
@@ -0,0 +1,11 @@
+let convictions = []
+
+export const useConvictions = () => convictions.slice()
+
+export const getConvictions = () => {
+    /*
+        Load database state into application state with a fetch().
+        Make sure the last `then()` sets the local `convictions`
+        variable to what is in the response from the API.
+    */
+}
