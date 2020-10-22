e7c3ffb32e0970b3bb5a5a2d56690d1f86571d5f
David Bing <davidb.devops@gmail.com>
2020-10-22 15:03:17 -0500

    module update: import and invoke NoteList()

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,7 @@
 import { ConvictionSelect } from './convictions/ConvictionSelect.js';
 import { CriminalList } from './criminals/CriminalList.js';
 import { NoteForm } from './notes/NoteForm.js';
+import { NoteList } from './notes/NoteList.js';
 import { OfficerList } from './officers/OfficerList.js'
 import {OfficerSelect} from './officers/OfficerSelect.js'
 
@@ -11,6 +12,7 @@ ConvictionSelect();
 CriminalList();
 OfficerSelect();
 NoteForm();
+NoteList();
 
 
 
