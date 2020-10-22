6790f1651e0ccc3938185539313b0ee281d2070b
David Bing <davidb.devops@gmail.com>
2020-10-22 15:12:40 -0500

    new module: NoteHTML.js - implements NoteHTML() to return HTML for indv. note.

---
+++ scripts/notes/NoteHTML.js
@@ -0,0 +1,12 @@
+export const NoteHTML = (noteObj) => {
+    return `
+    <section class="note__card">
+            <h3 class="note__suspect"> Suspect: ${noteObj.suspect}</h3>
+            <p class="note__name"> Officer: ${noteObj.author}</p>
+            <p class="note__conviction"> Note: ${noteObj.note}</p>
+            <p class="note__date"> Date: ${new Date(noteObj.timestamp).toLocaleDateString('en-US')}</p>
+    </section>
+    `
+}
+
+
