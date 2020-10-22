c35acebd6695d865f8af73c9ef9dc1ccdee17e9f
David Bing <davidb.devops@gmail.com>
2020-10-22 15:11:29 -0500

    new module: NoteForm.js - implements render(), NoteForm() and event listener to capture click events

---
+++ scripts/notes/NoteForm.js
@@ -0,0 +1,46 @@
+import { saveNote } from "./NoteProvider.js"
+
+const contentTarget = document.querySelector(".noteFormContainer")
+const eventHub      = document.querySelector(".container")
+
+// render form fields to the DOM
+const render = () => {
+    contentTarget.innerHTML = `
+    <input id="note--interviewDate" type="date"/>
+    <input id="note--author" type="text" placeholder="Your name here"/>
+    <input id="note--suspect" type="text" placeholder="suspect name here"/><br>
+    <textarea id="note--note" type="text" rows="5" cols="60"> </textarea><br>
+    <button id="saveNote">Save Note</button>
+    `
+}
+
+// event listener to capture click events
+eventHub.addEventListener("click", clickEvent => {
+    // console.log(clickEvent)
+    // ensure click has id of saveNote
+    if(clickEvent.target.id === "saveNote") {
+        // grab input values
+        const interviewDate = document.querySelector("#note--interviewDate").value
+        const timestamp     = Date.now()
+        const author        = document.querySelector("#note--author").value
+        const suspect       = document.querySelector("#note--suspect").value
+        const note          = document.querySelector("#note--note").value
+        
+        // and make a note object
+        const newNote = {
+            "date"      : interviewDate,
+            "timestamp" : timestamp,
+            "author"    : author,
+            "suspect"   : suspect,
+            "note"      : note
+        }
+        // console.log(newNote);
+        // send to json-server
+        saveNote(newNote)
+    }
+})
+
+// export function that calls render()
+export const NoteForm = () => {
+    render()
+}
