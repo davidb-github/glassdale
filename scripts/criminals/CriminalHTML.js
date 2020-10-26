d6411834bfdf0fe32181ea4e413c88a30ad322dc
David Bing <davidb.devops@gmail.com>
2020-10-26 14:08:00 -0500

    update module: CriminalHTML.js - update CriminalHTML() component with criminalObjID and implement eventHub/listener

--- scripts/criminals/CriminalHTML.js
+++ scripts/criminals/CriminalHTML.js
@@ -1,15 +1,40 @@
 /**
  *  CriminalHTML which renders individual criminal objects as HTML
  */
-export const CriminalHTML = (criminalObj) => {
 
+//  hang eventHub off of top level element in index.html.
+ const eventHub = document.querySelector(".container")
+
+export const CriminalHTML = (criminalObj) => {
     return `
-        <section class="criminal__card">
+        <section class="criminal__card" id="criminal-${criminalObj.id}">
             <h2 class="criminal__name">${criminalObj.name}</h2>
             <div class="criminal__age">Age: ${criminalObj.age}</div>
             <div class="criminal__conviction">Crime: ${criminalObj.conviction}</div>
             <div class="criminal__conviction">Term start: ${new Date(criminalObj.incarceration.start).toLocaleDateString('en-US')}</div>
             <div class="criminal__conviction">Term end: ${new Date(criminalObj.incarceration.end).toLocaleDateString('en-US')}</div>
+            <button id="associates--${criminalObj.id}">Associate Alibis</button>   
         </section>
     `
 }
+
+// listen for event
+eventHub.addEventListener("click", (eventObj) => {
+    // split button id string : <button id="associates--$">Associate Alibis</button>
+    const [prefix, criminalId] = eventObj.target.id.split("--")
+    
+    // check if event obj target id begins with...
+    if (eventObj.target.id.startsWith("associates--") ) {
+     // debug
+    //  console.log(prefix, criminalId)
+    
+    // build custom event
+    const myCustomEvent = new CustomEvent("alibiButtonSelected", {
+        detail: {
+            criminalId: criminalId
+        }
+    })
+        // dispatch custom event to eventHub
+        eventHub.dispatchEvent(myCustomEvent)
+    }
+})
