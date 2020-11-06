ec85b81cccfc8c0ab731dbde6779e73fdb418c1f
David Bing <davidb.devops@gmail.com>
2020-11-06 07:15:01 -0600

    update module: NoteHTML.js - update section element with criminalID for testing

--- scripts/notes/NoteHTML.js
+++ scripts/notes/NoteHTML.js
@@ -1,7 +1,7 @@
 export const NoteHTML = (noteObj) => {
     return `
     <section class="note__card">
-            <h3 class="note__suspect"> Suspect: ${noteObj.suspect}</h3>
+            <h3 class="note__suspect"> Suspect: ${noteObj.criminalId}</h3>
             <p class="note__name"> Officer: ${noteObj.author}</p>
             <p class="note__conviction"> Note: ${noteObj.note}</p>
             <p class="note__date"> Date: ${new Date(noteObj.timestamp).toLocaleDateString('en-US')}</p>
