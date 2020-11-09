c846c501ba5f1d2d9b6606bda27be5329e490f66
David Bing <davidb.devops@gmail.com>
2020-11-09 12:10:36 -0600

    update module: NoteList.js - refactor to call NoteHTML component to maintain separation of concerns

--- scripts/notes/NoteList.js
+++ scripts/notes/NoteList.js
@@ -28,38 +28,38 @@ export const NoteList = () => {
 Is it better to call .map and paint the html directly and get rid of the noteHTML.js module?
 Leaving original render function that calls noteHTML() until after discussion
 */
-// const render = (notesArray, criminalsArray) => {
-//   let notesHTMLRep = ""
+const render = (notesArray, criminalsArray) => {
+  let notesHTMLRep = ""
   // debugger
 
-//   for (let note of notesArray) {
-//     const mappedCriminal = criminalsArray.find(criminal => criminal.id === note.criminalId)
+  for (let note of notesArray) {
+    const mappedCriminal = criminalsArray.find(criminal => criminal.id === note.criminalId)
 
-//     notesHTMLRep += NoteHTML(note, mappedCriminal)
-//   }
-//   notesContainer.innerHTML = `
-//     <h3>Case Notes</h3>
-//       ${notesHTMLRep}
-//   `
-// }
+    notesHTMLRep += NoteHTML(note, mappedCriminal)
+  }
+  notesContainer.innerHTML = `
+    <h3>Case Notes</h3>
+      ${notesHTMLRep}
+  `
+}
 
 // maps through notesArray and puts HTML directly on the DOM.
-const render = (notesArray, criminalsArray) => {
-  notesContainer.innerHTML = notesArray.map(note => {
-      // Find the related criminal
-      const relatedCriminal = criminalsArray.find(criminal => criminal.id === note.criminalId)
+// const render = (notesArray, criminalsArray) => {
+//   notesContainer.innerHTML = notesArray.map(note => {
+//       // Find the related criminal
+//       const relatedCriminal = criminalsArray.find(criminal => criminal.id === note.criminalId)
 
-      return `
-<section class="note__card">
-  <p class="note__name"> Officer: ${note.author}</p>
-  <p class="note__suspect">Suspect: ${relatedCriminal.name} </p>
-  <p class="note__conviction"> Note: ${note.note}</p>
-  <p class="note__date"> Date: ${new Date(note.timestamp).toLocaleDateString('en-US')}</p>
-  <button id="deleteNote--${note.id}">Delete</button>
-</section>
-      `
-  }).join("")
-}
+//       return `
+// <section class="note__card">
+//   <p class="note__name"> Officer: ${note.author}</p>
+//   <p class="note__suspect">Suspect: ${relatedCriminal.name} </p>
+//   <p class="note__conviction"> Note: ${note.note}</p>
+//   <p class="note__date"> Date: ${new Date(note.timestamp).toLocaleDateString('en-US')}</p>
+//   <button id="deleteNote--${note.id}">Delete</button>
+// </section>
+//       `
+//   }).join("")
+// }
 
 eventHub.addEventListener("click", clickEvent => {
   if (clickEvent.target.id.startsWith("deleteNote--")) {
